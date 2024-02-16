2cccd9b9d3f209aa67adf0792d17c6729ffbbc2a
nearbystays <86144003+nearbystays@users.noreply.github.com>
2024-02-16 09:18:13 +0000

    Jinx

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ if (!extension_loaded('trader')) {
     }
 
     // Load data from file
-    $data = file_get_contents('data.jinx.txt');
+    $data = file_get_contents('~/.jinx.txt');
     $rows = explode("\n", $data);
     $closePrices = array_map('floatval', $rows);
 
@@ -14,22 +14,22 @@ if (!extension_loaded('trader')) {
     $ema = trader_ema($closePrices, 10);
     $macd = trader_macd($closePrices, 12, 26, 9);
     $rsi = trader_rsi($closePrices, 14);
-    $adx = trader_adx($closePrices, 14);
-    $cci = trader_cci($closePrices, 20);
-    $atr = trader_atr($closePrices, 14);
+    // $adx = trader_adx($closePrices, 14);
+    // $cci = trader_cci($closePrices, 20);
+    // $atr = trader_atr($closePrices, 14);
     $bbands = trader_bbands($closePrices, 20);
     $stoch = trader_stoch($closePrices, $closePrices, $closePrices, 14);
-    $willr = trader_willr($closePrices, $closePrices, $closePrices, 14);
+    // $willr = trader_willr($closePrices, $closePrices, $closePrices, 14);
 
     // Print results
     print_r($sma);
     print_r($ema);
     print_r($macd);
     print_r($rsi);
-    print_r($adx);
-    print_r($cci);
-    print_r($atr);
+    // print_r($adx);
+    // print_r($cci);
+    // print_r($atr);
     print_r($bbands);
     print_r($stoch);
-    print_r($willr);
+    // print_r($willr);
 ?>
