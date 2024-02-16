e2acfef7f213df54596b6a5a61ff86fdbce1d0f2
nearbystays <86144003+nearbystays@users.noreply.github.com>
2024-02-16 10:27:24 +0000

    Jin

---
+++ index.py
@@ -0,0 +1,46 @@
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense
+
+# Load the data
+data = pd.read_csv('nasdaq.txt', header=None)
+
+# Preprocess the data
+scaler = MinMaxScaler(feature_range=(0, 1))
+scaled_data = scaler.fit_transform(data)
+
+# Create sequences of 60 values (you can adjust this value)
+X = []
+y = []
+for i in range(60, len(scaled_data)):
+    X.append(scaled_data[i-60:i, 0])
+    y.append(scaled_data[i, 0])
+X, y = np.array(X), np.array(y)
+
+# Reshape the data
+X = np.reshape(X, (X.shape[0], X.shape[1], 1))
+
+# Create the model
+model = Sequential()
+model.add(LSTM(units=50, return_sequences=True, input_shape=(X.shape[1], 1)))
+model.add(LSTM(units=50))
+model.add(Dense(1))
+
+# Compile and train the model
+model.compile(loss='mean_squared_error', optimizer='adam')
+model.fit(X, y, epochs=1, batch_size=1, verbose=2)
+
+# Predict the next price
+inputs = scaled_data[-60:]
+inputs = inputs.reshape(-1,1)
+inputs = scaler.transform(inputs)
+X_test = []
+X_test.append(inputs[0:60, 0])
+X_test = np.array(X_test)
+X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
+predicted_price = model.predict(X_test)
+predicted_price = scaler.inverse_transform(predicted_price)
+
+print(predicted_price)
